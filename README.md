5cc5330b3c6f51e774ea6e4772a9e9dad4ac5fbd
Geoff Hulette <geoff@hulette.net>
2021-03-07 19:42:01 -0800

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+Implementing the DSL described here:
+
+https://retrocomputing.stackexchange.com/questions/13897/why-was-the-kickstart-1-x-insert-floppy-graphic-so-bad
--- src/draw.ml
+++ src/draw.ml
@@ -1,17 +1,38 @@
-let draw_polyline _ _ = ()
-let flood_fill _ _ = ()
+
+let set_color =
+  let rgb r g b = Graphics.rgb (r * 16) (g * 16) (b * 16) in
+  function
+  | '\x00' -> Graphics.set_color (rgb 0xF 0xF 0xF)
+  | '\x01' -> Graphics.set_color (rgb 0x0 0x0 0x0)
+  | '\x02' -> Graphics.set_color (rgb 0x7 0x7 0xC)
+  | '\x03' -> Graphics.set_color (rgb 0xB 0xB 0xB)
+  | _ -> failwith "unexpected color index"
+
+let jump x y = Graphics.moveto (Char.code x) (Char.code y)
+let line_to x y = Graphics.lineto (Char.code x) (Char.code y)
+let fill x y = Graphics.plot (Char.code x) (Char.code y)
+
+type mode = Jump | Line | Fill
+
+let rec interp ch mode =
+  let b1 = input_char ch in
+  let b2 = input_char ch in
+  match mode, b1, b2 with
+  | _, '\xFF', '\xFF' -> ()
+  | _, '\xFF', col -> set_color col; interp ch Jump
+  | _, '\xFE', col -> set_color col; interp ch Fill
+  | Jump, x, y -> jump x y; interp ch Line
+  | Line, x, y -> line_to x y; interp ch Line
+  | Fill, x, y -> fill x y; interp ch Fill
+
+let interp_channel ch =
+  try interp ch Jump
+  with End_of_file -> failwith "unexpected EOF"
 
 let () =
-  Graphics.open_graph "";
-  (* Graphics.set_window_title "Amiga Kickstart";
-  Graphics.resize_window 320 200; *)
-  let done_ = ref false in
-  while !done_ do
-    Graphics.clear_graph ();
-    Graphics.synchronize ();
-    let status = Graphics.(wait_next_event [Key_pressed]) in
-    if status.Graphics.keypressed then begin
-      done_ := true;
-      print_endline "exiting"
-    end
-  done
+  let open Graphics in
+  open_graph "";
+  set_window_title "Kickstart";
+  resize_window 320 200;
+  interp_channel stdin;
+  loop_at_exit [Key_pressed] (fun st -> if st.keypressed then raise Exit)
